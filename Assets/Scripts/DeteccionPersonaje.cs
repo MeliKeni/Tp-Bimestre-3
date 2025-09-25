a815638430dea6238985546f1abfb874f6d520ce
MeliKeni <melinakenigsberg@gmail.com>
2025-09-25 11:00:20 -0300

    nose

---
+++ Assets/Scripts/DeteccionPersonaje.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastSight : MonoBehaviour
+{
+    [SerializeField] Transform originTR;
+    [SerializeField] float rayLenght;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originTR = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(originTR.position, originTR.forward, out hitInfo, rayLenght))
+        {
+            Debug.Log(hitInfo.collider.name);
+
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Color color = Color.red;
+        Gizmos.color = color;
+        Gizmos.DrawLine(originTR.position, originTR.position + originTR.forward * rayLenght);
+
+    }
+}
+
